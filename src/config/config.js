25a77b9485c6725f096dee4b691798fdb161f0c8
Erick Furtado <erickf.furtado@outlook.com>
2023-03-27 21:05:38 -0300

    new config and cloud to bot

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'MTA4ODY1Mzk1MTEwMjA5NTQzMA.GOnFI0.Rm74hUvNOeKRHst3cNCaJIbS4J0haI2wNMUhr0',
+    token: 'MTA4ODY1Mzk1MTEwMjA5NTQzMA.GUQHfj.mUSV4qNDqGo9m4oY5htXEzmmgMh3f-mGzsoygA',
     prefix: '=',
     createRoomCategory: 'Nome da categoria para criar novas salas',
   };
